9d9b06726e0ce2650b561cd7b6aad29ef3a33324
MishkaRogachev <mishkarogachev@gmail.com>
2021-10-08 14:41:29 +0300

    Move missions logic to kjarni

--- api/src/mission_type.cpp
+++
@@ -1,9 +0,0 @@
-#include "mission_type.h"
-
-using namespace md::domain;
-
-MissionType::MissionType(const QString& name, const RouteType& routeType) :
-    name(name),
-    routeType(routeType)
-{
-}
--- api/src/route_type.cpp
+++
@@ -1,9 +0,0 @@
-#include "route_type.h"
-
-using namespace md::domain;
-
-RouteType::RouteType(const QString& name, const QVector<WaypointType>& waypointTypes) :
-    name(name),
-    waypointTypes(waypointTypes)
-{
-}
